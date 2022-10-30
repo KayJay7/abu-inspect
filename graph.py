1964a5b7efe6e841a47a8d6145d37d5aa6c85e30
Alvise <bruniera.alvise@spes.uniud.it>
2022-10-30 17:33:56 +0100

    Fixed the digestion script

--- graph.py
+++ graph.py
@@ -6,33 +6,55 @@ import copy as cp
 import re
 import csv
 
+############ Configuration
+# These are filenames and other data that's been hardcoded into the digest script
+# Tweak these if you want to digest different tests
+
+# Results filenames, these are found in the './results' directory
 all_files = ["idle_10_500_5.json", "idle_50_500_5.json", "idle_100_500_5.json", "low_10_610_5.json", "low_50_630_5.json", "low_100_1030_5.json", "high_10_505_5.json", "high_50_620_5.json", "high_100_2000_5.json"]
+# Only the files of logged tests
+logged_files = ["low_10_610_5.json", "low_50_630_5.json", "low_100_1030_5.json", "high_10_505_5.json", "high_50_620_5.json", "high_100_2000_5.json"]
+# Running time of each test
 all_times = [500, 500, 500, 610, 630, 1030, 505, 620, 2000]
+# Folders inside the './results/log-digest' directory, they contains splitted log files numbered 1-n
 all_folders = ["low10", "low50", "low100", "high10", "high50", "high100"]
+# Labels for graphs and csv files
+all_labels = ["Idle 10", "Idle 50", "Idle 100", "Low 10", "Low 50", "Low 100", "High 10", "High 50", "High 100"]
+# Only the labels concerned with logs data
+logs_labels = ["Low 10", "Low 50", "Low 100", "High 10", "High 50", "High 100"]
+# Which agent has been logged
+logged_agent = "abusim-example-agent0"
+# In how many parts is split each log (how many iterations per test)
+log_parts = 5
+
+############ End of configuration
+
+# All metrics with descriptions
 template = {
-    "received bytes": [],
-    "received packets": [],
-    "sent bytes": [],
-    "sent packets": [],
-    "received bytes per seconds": [],
-    "received packets per seconds": [],
-    "sent bytes per seconds": [],
-    "sent packets per seconds": [],
-    "bytes ratio": [],
-    "packets ratio": [],
-    "bytes per packet": [],
-    "total bytes": [],
-    "total packets": [],
-    "total bytes per seconds": [],
-    "total packets per seconds": []
+    "received bytes": [], # Total received bytes per agent
+    "received packets": [], # Total received packets per agent
+    "sent bytes": [], # Total sent bytes per agent
+    "sent packets": [], # Total sent packets per agent
+    "received bytes per seconds": [], # Like above, but divided by running time
+    "received packets per seconds": [], # Like above, but divided by running time
+    "sent bytes per seconds": [], # Like above, but divided by running time
+    "sent packets per seconds": [], # Like above, but divided by running time
+    "bytes ratio": [], # received/sent bytes
+    "packets ratio": [], # received/sent packets
+    "bytes per packet": [], # Average size of each packet
+    "total bytes": [], # Total bytes sent in the network by all agents
+    "total packets": [], # Total packets sent in the network by all agents
+    "total bytes per seconds": [], # Like above, but divided by running time
+    "total packets per seconds": [] # Like above, but divided by running time
 }
+# Log metrics with descriptions
 logs_template = {
-    "expected bytes": [],
-    "actual bytes": [],
-    "expected packets": [],
-    "actual packets": [],
-    "bytes ratio": [],
-    "packets ratio": []
+    "expected bytes": [], # Sent bytes as reported in the logs
+    "actual bytes": [], # Sent bytes as measured by the system
+    "expected packets": [], # Sent packets as reported in the logs
+    "actual packets": [], # Sent packets as measured by the system
+    "bytes ratio": [], # actual/expected bytes
+    "packets ratio": [] # actual/expected packets
 }
 
 def process(raw_data, time):
@@ -71,7 +93,7 @@ def read_json(name, time):
 def collect_data():
     data = cp.deepcopy(template)
 
-    for i in range(0, 9):
+    for i in range(0, len(all_files)):
         test = read_json(f"results/{all_files[i]}", all_times[i])
         for metric in data:
             data[metric].append(test[metric])
@@ -83,16 +105,16 @@ def read_agent0(name):
     data = []
     raw_data = json.load(f)
     for sample in raw_data:
-        data.append(sample["abusim-example-agent0"])
+        data.append(sample[logged_agent])
     f.close()
     return data
 
 def digest_logs():
     data = cp.deepcopy(logs_template)
-    for i in range(0, 6):
+    for i in range(0, len(logs_labels)):
         partial_data = cp.deepcopy(logs_template)
-        test = read_agent0(f"results/{all_files[i]}")
-        for j in range(1,6):
+        test = read_agent0(f"results/{logged_files[i]}")
+        for j in range(1, log_parts+1):
             expected_bytes = 0
             expected_packets = 0
             file = open(f"results/log-digest/{all_folders[i]}/{j}.log", "r")
@@ -139,27 +161,25 @@ def write_data_csv(data, labels, prefix):
             write_table_csv(writer_totals, total_metrics, totals)
 
 def main():
-    labels = ["Idle 10", "Idle 50", "Idle 100", "Low 10", "Low 50", "Low 100", "High 10", "High 50", "High 100"]
     data = collect_data()
     for metric in data:
         plt.clf()
-        plt.boxplot(data[metric], labels = labels, )
+        plt.boxplot(data[metric], labels = all_labels, )
         plt.title(metric)
         plt.autoscale()
         plt.savefig(f"graphs/{metric}.png", dpi=300)
 
-    write_data_csv(data, labels, "")
+    write_data_csv(data, all_labels, "")
     
-    labels = ["Low 10", "Low 50", "Low 100", "High 10", "High 50", "High 100"]
     logs_data = digest_logs()
     for metric in logs_data:
         plt.clf()
-        plt.boxplot(logs_data[metric], labels = labels)
+        plt.boxplot(logs_data[metric], labels = logs_labels)
         plt.title(metric)
         plt.autoscale()
         plt.savefig(f"graphs/logs_{metric}.png", dpi=300)
 
-    write_data_csv(logs_data, labels, "logs_")
+    write_data_csv(logs_data, logs_labels, "logs_")
     
     
 
